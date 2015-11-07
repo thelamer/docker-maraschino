fe8b4010c0348945bbc4c127584098763c1babb1
smdion <smdion@gmail.com>
2015-11-07 16:18:08 -0600

    run git pull as abc

--- init/30_git_update.sh
+++ init/30_git_update.sh
@@ -4,5 +4,5 @@ if [ ! -d "/app/maraschino/.git" ]; then
 /sbin/setuser abc git clone https://github.com/mrkipling/maraschino.git /app/maraschino
 else
 cd /app/maraschino
-git pull
+/sbin/setuser abc git pull
 fi
